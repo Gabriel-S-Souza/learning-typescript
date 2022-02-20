1528d1a6b0a2b564716bd03b52443d632a6947f9
Gabriel S. Souza <gabriel12saraiva@gmail.com>
2022-02-20 11:18:02 -0300

    created new interfaces and implemented in the EmailService class

--- src/services/EmailService.ts
+++ src/services/EmailService.ts
@@ -8,8 +8,18 @@ interface IEmailMessage {
     attachment?: string[] // o ? é para dizer que esse parâmetro é opicional e o [] é para dizer que é uma array (nesse caso uma array de strings)
 }
 
-class EmailService {
-    sendEmail(to: IEmailTo, message: IEmailMessage) { //
+interface IMessageDTO {
+    to: IEmailTo,
+    message: IEmailMessage
+}
+
+//DTO: Data Transfer Object (Conceito da Metodologia DDD)
+
+interface IEmailService {
+    sendEmail(request: IMessageDTO): void
+}
+class EmailService implements IEmailService {
+    sendEmail({to, message}: IMessageDTO) { //
         return `Email enviado para ${to.name}: assunto: ${message.subject}, mensagem: ${message.content}`
     }
 }
