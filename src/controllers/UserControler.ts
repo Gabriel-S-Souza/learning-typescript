f96cc1daa7329e438820ba955ac97db046fbdcd6
Gabriel S. Souza <gabriel12saraiva@gmail.com>
2022-02-20 15:55:44 -0300

    Added cors in backend app

--- src/controllers/UserControler.ts
+++ src/controllers/UserControler.ts
@@ -20,8 +20,8 @@ export default {
             },
 
             message: {
-                    subject: "Saudações",
-                    content: "Seja bem vindo ao sistema" 
+                subject: "Saudações",
+                content: "Seja bem vindo ao sistema" 
                 }
         })
 
